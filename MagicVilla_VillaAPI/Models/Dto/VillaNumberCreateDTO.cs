8bc07e8b0ad0455658f16c0d33f8908e5566abb9
TuanND <tuannd1058@gmail.com>
2023-05-09 08:58:29 +0700

    Villa Number Models

---
+++ MagicVilla_VillaAPI/Models/Dto/VillaNumberCreateDTO.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MagicVilla_VillaAPI.Models.Dto
+{
+    public class VillaNumbercreateDTO
+    {
+        [Required]
+        public int VillaNo { get; set; }
+        public string SepcialDetails { get; set; }
+    }
+}
