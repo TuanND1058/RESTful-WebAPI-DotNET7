6ba8d0c279a34900a69f8b07cc81b7abc2dc1038
TuanND <tuannd1058@gmail.com>
2023-05-11 15:21:39 +0700

    Add User Repository

---
+++ MagicVilla_VillaAPI/Repository/UserRepository.cs
@@ -0,0 +1,32 @@
+﻿using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.Dto;
+using MagicVilla_VillaAPI.Repository.IRepository;
+
+namespace MagicVilla_VillaAPI.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public bool IsUniqueUser(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LoginRequestDTO> Login(LoginRequestDTO request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LocalUser> Register(RegisterationRequestDTO request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
