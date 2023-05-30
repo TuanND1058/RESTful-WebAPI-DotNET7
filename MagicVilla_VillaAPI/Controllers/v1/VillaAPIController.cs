25b360082e47349e0d64bdcde741d890c5ed87b3
TuanND <tuannd1058@gmail.com>
2023-05-30 13:33:45 +0700

    Pagination in API

--- MagicVilla_VillaAPI/Controllers/v1/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/v1/VillaAPIController.cs
@@ -30,7 +30,8 @@ namespace MagicVilla_VillaAPI.Controllers.v1
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status403Forbidden)]
-        public async Task<ActionResult<APIResponse>> GetVillasAsync([FromQuery(Name = "filterOccupancy")] int? occupancy, [FromQuery] string? search)
+        public async Task<ActionResult<APIResponse>> GetVillasAsync([FromQuery(Name = "filterOccupancy")] int? occupancy,
+            [FromQuery] string? search, int pageSize = 2, int pageNumber = 1)
         {
             try
             {
@@ -38,11 +39,11 @@ namespace MagicVilla_VillaAPI.Controllers.v1
 
                 if (occupancy > 0)
                 {
-                    villaList = await _villaRepository.GetAllAsync(x => x.Occupancy == occupancy);
+                    villaList = await _villaRepository.GetAllAsync(x => x.Occupancy == occupancy, pageSize: pageSize, pageNumber: pageNumber);
                 }
                 else
                 {
-                    villaList = await _villaRepository.GetAllAsync();
+                    villaList = await _villaRepository.GetAllAsync(pageSize: pageSize, pageNumber: pageNumber);
                 }
 
                 if (!string.IsNullOrEmpty(search))
